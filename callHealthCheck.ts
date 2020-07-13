cd0e66ef78df4de5534cce02138a3cb9b7170c26
Joel Cripps <jcripps@fortinet.com>
2020-07-13 08:54:28 -0700

    fix review comments

--- callHealthCheck.ts
+++ callHealthCheck.ts
@@ -1,33 +1,34 @@
-import axios from 'axios';
-import https from 'https';
+import axios from "axios";
+import https from "https";
 
-const {
-    FULL_URL,
-    API_KEY
-} = process.env;
+const { FULL_URL, API_KEY } = process.env;
 
-exports.callHealthCheck = async function(event, context, callback):Promise<void>{
-        console.log(`Timer Function Called`)
-        var url =  FULL_URL;
-        const agent = new https.Agent({
-            rejectUnauthorized: false
-        });
-        var options = {
-            httpsAgent: agent,
-            headers: {
-                Authorization: 'Bearer ' +  API_KEY
-            }
-        };
-        try {
-            console.log(`Calling ${FULL_URL}`)
-            await axios.get(url, options);
-        } catch (err) {
-            throw console.error(`Error in CallHealthCheck Function. Error : ${err} `);
-        }
-        // Terminate function.
-        callback(null, 'CallHealthCheck Terminated.');
-
-    }
+exports.callHealthCheck = async function(
+  event,
+  context,
+  callback
+): Promise<void> {
+  console.log(`Timer Function Called`);
+  var url = FULL_URL;
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+  var options = {
+    httpsAgent: agent,
+    headers: {
+      Authorization: "Bearer " + API_KEY,
+    },
+  };
+  try {
+    console.log(`Calling ${FULL_URL}`);
+    await axios.get(url, options);
+  } catch (err) {
+    console.error(`Error in CallHealthCheck Function. Error : ${err} `);
+    callback(err);
+  }
+  // Terminate function.
+  callback(null, "CallHealthCheck Terminated.");
+};
 if (module === require.main) {
-    exports.callHeallthCheck(console.log);
+  exports.callHeallthCheck(console.log);
 }
